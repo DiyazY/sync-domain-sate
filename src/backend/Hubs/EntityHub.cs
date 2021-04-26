bf39f6a9d210b4de87eb8e7c3984880b6ee00248
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2021-04-26 22:52:13 +0300

    added signalR hub and 2 dummy endpoints

---
+++ src/backend/Hubs/EntityHub.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace backend.Hubs
+{
+    public class EntityHub : Hub
+    {
+    }
+}
--- src/backend/Startup.cs
+++ src/backend/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,7 +18,32 @@ namespace backend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
+            services.AddCors(options => 
+                options.AddPolicy("cors", builder =>
+                {
+                    builder
+                        .SetIsOriginAllowed(_ => true)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                }));
         }
+
+        class Entity
+        {
+            public int Id { get; set; }
+            public DateTime Time { get; set; }
+            public override string ToString() => $"{{id:{Id}, time:{Time}}}";
+        }
+
+        private List<Entity> _entities = new List<Entity>
+        {
+            new Entity {Id = 1, Time = DateTime.Now},
+            new Entity {Id = 2, Time = DateTime.Now},
+            new Entity {Id = 3, Time = DateTime.Now},
+            new Entity {Id = 4, Time = DateTime.Now}
+        };
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -25,14 +52,29 @@ namespace backend
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors("cors");
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapHub<EntityHub>("/entityHub");
+                
+                endpoints.MapGet("/get-entity",
+                    async context =>
+                    {
+                        var id = int.Parse(context.Request.Query["id"]);
+                        var entity = _entities.FirstOrDefault(p => p.Id == id);
+                        await context.Response.WriteAsync(entity.ToString());
+                    });
+
+                endpoints.MapGet("/trigger-update-entity", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    var id = int.Parse(context.Request.Query["id"]);
+                    var entity = _entities.FirstOrDefault(p => p.Id == id);
+                    entity.Time = DateTime.Now;
+                    var hub = app.ApplicationServices.GetService<IHubContext<EntityHub>>();
+                    hub?.Clients.All.SendAsync("updated-entity", id);
+                    await context.Response.WriteAsync(entity.ToString());
                 });
             });
         }
